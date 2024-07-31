955bab3db7dd0e1e27c2910c65e9e545c5492397
macyjoe <macyjoe15@gmail.com>
2024-07-31 17:04:16 +0900

    work

--- src/test/java/com/ll/sapp/SappApplicationTests.java
+++ src/test/java/com/ll/sapp/SappApplicationTests.java
@@ -1,13 +1,48 @@
 package com.ll.sapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private QuestionRepository questionRepository;
 
+    @Test
+    void testJpa() {
+        // 1st
+//		Question q1 = new Question();
+//		q1.setSubject("sbb가 무엇인가요?");
+//		q1.setContent("sbb에 대해서 알고 싶습니다.");
+//		q1.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q1);  // 첫번째 질문 저장
+//
+//		Question q2 = new Question();
+//		q2.setSubject("스프링부트 모델 질문입니다.");
+//		q2.setContent("id는 자동으로 생성되나요?");
+//		q2.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q2);  // 두번째 질문 저장
+
+        // 2nd
+//		List<Question> all = this.questionRepository.findAll();
+//		assertEquals(2, all.size());
+//
+//		Question q = all.get(0);
+//		assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+        //3rd
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		if(oq.isPresent()) {
+//			Question q = oq.get();
+//			assertEquals("sbb가 무엇인가요?", q.getSubject());
+//		}
+
+        //4th
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
+    }
 }
